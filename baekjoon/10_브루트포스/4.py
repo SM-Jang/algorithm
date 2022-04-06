6fd8bc2a0a5b175ea7bf772fcebb31cb9c40cc68
SM <smsm8898@ajou.ac.kr>
2022-04-06 14:35:59 +0900

    0406

--- baekjoon/10_브루트포스/4.py
+++ baekjoon/10_브루트포스/4.py
@@ -1,25 +1,48 @@
-# 1. 입력(체스판) -> 2차원 행렬
-row, col = map(int, input().split())
+# 1. 입력
+n,m = map(int, input().split())
 chess = []
-for _ in range(row):
-    chess.append(input()) # 문자열 입력
+for _ in range(n):
+    chess.append(input())
+
+
+
+# 2. 8x8 체스판 유형 2가지(메모)
+
+def check_chess_8x8(chess):
+    type1='BWBWBWBW'
+    type2='WBWBWBWB'
+    repair1 = 0
+    repair2 = 0
     
-# 8x8씩 확인
-repair = []
-for r in range(row-7):
-    for c in range(col-7):
-        # 2. 시작 색 확인
-        init1=0 #'W'
-        init2=0 #'B'
-        for i in range(r, r+8):
-            for j in range(c, c+8):
-                # 2. 시작 색 확인
-                if (i+j) % 2 == 0:
-                    if chess[i][j] != 'W': init1+=1
-                    if chess[i][j] != 'B': init2+=1
-                else:
-                    if chess[i][j] != 'W': init2+=1
-                    if chess[i][j] != 'B': init1+=1
-        repair.append(min(init1, init2))
+    for row in range(8):
+        chess_row = chess[row]
+        for col in range(8):
+            chess_row_col = chess_row[col]
+            if row % 2 == 0:
+                if chess_row_col != type1[col]: repair1 += 1# type1
+                if chess_row_col != type2[col]: repair2 += 1# type2
+            else:
+                if chess_row_col != type2[col]: repair1 += 1# type2
+                if chess_row_col != type1[col]: repair2 += 1# type1
+    return min(repair1, repair2)
+
+
+
+# 3. 완전 탐색(수리횟수)
+ans = []
+for i in range(n-8+1):
+    for j in range(m-8+1):
+        # 8x8로 자르기
+        chess_8x8 = []
+        chess_rows = chess[i:i+8]
+        for chess_row in chess_rows:
+            chess_8x8.append(chess_row[j:j+8])
+
+        # 고쳐야할 부분 찾기
+        repair = check_chess_8x8(chess_8x8)
         
-print(min(repair))
+        ans.append(repair)
+
+
+# 4. 최소 수리횟수 출력
+print(min(ans))
