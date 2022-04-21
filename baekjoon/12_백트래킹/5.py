c4823f6294835d395e2f69f8c19d2f03d4be870f
SM <smsm8898@ajou.ac.kr>
2022-04-21 11:45:30 +0900

    0421

--- baekjoon/12_백트래킹/5.py
+++ baekjoon/12_백트래킹/5.py
@@ -1,32 +1,32 @@
-# https://velog.io/@hope1213/%EB%B0%B1%EC%A4%80-9663-N-Queen-%ED%8C%8C%EC%9D%B4%EC%8D%AC
-n = int(input())
-
-graph = [0]*n
-visited = [0]*n
+def check(row):
+    for i in range(row):
+        # 2-1. 현 queen의 위치 (열)이 이전에 놓은 것과 겹치는 지(행은 절대 안겹침)
+        # 2-2. 대각으로 겹치는지
+        if qPos[row] == qPos[i] or abs(qPos[row]-qPos[i]) == (row-i):
+            return False
+    return True
 
 
 def dfs(depth):
-    global count
-    if n == depth:
-        count+=1
+    global cnt
+    # 종료 조건
+    if depth >= n: 
+        cnt += 1
         return
     
     for i in range(n):
-        if not visited[i]:
-            graph[depth]=i
-            t=True
-            
-            for j in range(depth):
-                if abs(graph[depth]-graph[j]) == abs(depth-j):
-                    t=False
-                    break
-                    
-            if t:
-                # print(graph, depth)
-                visited[i]=1
-                dfs(depth+1)
-                visited[i]=0
-                
-count=0
+        # 1. 위치에 퀸 놓기
+        qPos[depth] = i
+        
+        # 2. 가능 위치 확인
+        if check(depth):
+            dfs(depth+1)
+
+
+# main
+n=int(input())
+cnt = 0
+qPos = [-1] * n # (index, value) = (행, 열)
 dfs(0)
-print(count)
+    
+print(cnt)
