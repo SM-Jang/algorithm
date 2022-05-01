950d94905485eda766609ee83a3b0c343aeb0ac3
SM <smsm8898@ajou.ac.kr>
2022-05-01 13:33:11 +0900

    0501

--- baekjoon/13_DP/1463.py
+++ baekjoon/13_DP/1463.py
@@ -24,3 +24,16 @@ def bfs(n):
 n = int(input())
 cnt = bfs(n)
 print(cnt)
+
+
+# DP
+n=9
+d = [0]*(n+1)
+
+for i in range(2, n+1):
+    d[i] = d[i-1]+1
+    if i % 2 == 0:
+        d[i] = min(d[i//2]+1, d[i])
+    if i % 3 == 0:
+        d[i] = min(d[i//3]+1, d[i])
+d[n]
